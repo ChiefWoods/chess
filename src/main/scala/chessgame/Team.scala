6059f1482e1d1e3f2cf0119bc197ac07acf57906
Chii Yuen <chii.yuen@hotmail.com>
2023-12-20 14:13:34 +0800

    Add Team values

--- src/main/scala/chessgame/Team.scala
+++ src/main/scala/chessgame/Team.scala
@@ -1,7 +1,17 @@
 package chessgame
 
 object Team extends Enumeration {
-  abstract class Team extends super.Val {
-    def getDirection: Int
+  type Team = Value
+  val WHITE, BLACK = Value
+
+  implicit class TeamOps(team: Value) {
+    def getDirection: Int = team match {
+      case WHITE => -1
+      case BLACK => 1
+    }
+
+    def isWhite: Boolean = team == WHITE
+
+    def isBlack: Boolean = team == BLACK
   }
 }
