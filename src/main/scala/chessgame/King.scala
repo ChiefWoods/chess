e2948f35fe60cf60a9359986978fd49d10420ba1
Chii Yuen <chii.yuen@hotmail.com>
2023-12-20 15:33:04 +0800

    Add King.scala

---
+++ src/main/scala/chessgame/King.scala
@@ -0,0 +1,43 @@
+package chessgame
+
+import chessgame.Team.Team
+
+class King(team: Team, piecePosition: Int) extends Piece(team, piecePosition) {
+  val CANDIDATE_MOVE_COORDINATES: List[Int] = List(-9, -8, -7, -1, 1, 7, 8, 9)
+
+  override def calculateLegalMoves(board: Board): Set[Move] = {
+    val legalMoves: Set[Move] = Set()
+
+    for (currentCandidateOffset <- CANDIDATE_MOVE_COORDINATES) {
+      val candidateDestinationCoordinate = piecePosition + currentCandidateOffset
+
+      if (!isFirstColumnExclusion(piecePosition, currentCandidateOffset) &&
+        !isEighthColumnExclusion(piecePosition, currentCandidateOffset)) {
+        if (Board.isValidTileCoordinate(candidateDestinationCoordinate)) {
+          val candidateDestinationTile: Tile = board.getTile(candidateDestinationCoordinate)
+
+          if (!candidateDestinationTile.isTileOccupied) {
+            legalMoves + MajorMove(board, this, candidateDestinationCoordinate)
+          } else {
+            val pieceAtDestination: Piece = candidateDestinationTile.getPiece
+            val pieceTeam: Team = pieceAtDestination.getPieceTeam
+
+            if (team != pieceTeam) {
+              legalMoves + AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination)
+            }
+          }
+        }
+      }
+    }
+
+    legalMoves
+  }
+
+  def isFirstColumnExclusion(currentPosition: Int, candidateOffset: Int): Boolean = {
+    Board.FIRST_COLUMN(currentPosition) && (candidateOffset == -9 || candidateOffset == -1 || candidateOffset == 7)
+  }
+
+  def isEighthColumnExclusion(currentPosition: Int, candidateOffset: Int): Boolean = {
+    Board.EIGHTH_COLUMN(currentPosition) && (candidateOffset == -7 || candidateOffset == 1 || candidateOffset == 9)
+  }
+}
