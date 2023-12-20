c93baa8794ef416e589bf2040adae95e02c2e114
Chii Yuen <chii.yuen@hotmail.com>
2023-12-20 14:12:34 +0800

    Add isFirstMove property

--- src/main/scala/chessgame/Piece.scala
+++ src/main/scala/chessgame/Piece.scala
@@ -2,8 +2,10 @@ package chessgame
 
 import chessgame.Team.Team
 
-abstract class Piece(private val team : Team, private val piecePosition : Int) {
+abstract class Piece(private val team : Team, private val piecePosition : Int, private val isFirstMove : Boolean = false) {
   def getPieceTeam: Team = team
+
+  def getFirstMove: Boolean = isFirstMove
 
   def calculateLegalMoves(board : Board) : Set[Move]
 }
