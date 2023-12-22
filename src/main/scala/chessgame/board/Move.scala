4d87b8b7eb83e5fb2f89d66dd0dbb8c1ab793bfd
Chii Yuen <chii.yuen@hotmail.com>
2023-12-23 05:32:08 +0800

    Move.scala fallback

--- src/main/scala/chessgame/board/Move.scala
+++ src/main/scala/chessgame/board/Move.scala
@@ -2,7 +2,7 @@ package chessgame.board
 
 import chessgame.pieces.{Pawn, Piece, Rook}
 
-abstract class Move(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int) {
+abstract class Move(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece = null, private val isFirstMove: Boolean = false) {
   def getDestinationCoordinate: Int = destinationCoordinate
 
   def getMovedPiece: Piece = movedPiece
@@ -34,20 +34,23 @@ abstract class Move(private val board: Board, private val movedPiece: Piece, pri
   }
 
   override def hashCode: Int = {
-    var result = 31 * 1 + movedPiece.getPiecePosition
-    result = 31 * result + destinationCoordinate
-    result = 31 * result + movedPiece.hashCode
+    val prime = 31
+    var result = 1
+    result = prime * result + destinationCoordinate
+    result = prime * result + movedPiece.hashCode
+    result = prime * result + movedPiece.getPiecePosition
 
     result
   }
 
-  def equals(obj: Move): Boolean = {
-    if (this == obj) return true
-    if (obj == null || !obj.isInstanceOf[Move]) return false
+  override def equals(obj: Any): Boolean = {
+//    if (this == obj) return true
+    if (!obj.isInstanceOf[Move]) return false
 
-    val move: Move = obj
+    val move: Move = obj.asInstanceOf[Move]
 
-    getDestinationCoordinate == move.getDestinationCoordinate &&
+    getCurrentCoordinate == move.getCurrentCoordinate &&
+      getDestinationCoordinate == move.getDestinationCoordinate &&
       getMovedPiece.equals(move.getMovedPiece)
   }
 }
@@ -67,21 +70,28 @@ object Move {
   }
 }
 
-class MajorMove(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int)
-  extends Move(board, movedPiece, destinationCoordinate) {
+class MajorMove(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece)
+  extends Move(board, destinationCoordinate, movedPiece) {
+
+  override def equals(obj: Any): Boolean = {
+//    this == obj || obj.isInstanceOf[MajorMove] && super.equals(obj)
+    obj.isInstanceOf[MajorMove] && super.equals(obj)
+  }
+
+  override def toString: String = {
+    movedPiece.getPieceType.toString + Board.getPositionAtCoordinate(destinationCoordinate)
+  }
 }
 
-class AttackMove(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int, private val attackedPiece: Piece)
-  extends Move(board, movedPiece, destinationCoordinate) {
+class AttackMove(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val attackedPiece: Piece)
+  extends Move(board, destinationCoordinate, movedPiece) {
   override def isAttack: Boolean = true
 
   override def getAttackedPiece: Piece = attackedPiece
 
-  override def execute: Board = null
-
   override def equals(obj: Any): Boolean = {
-    if (this == obj) return true
-    if (obj == null || !obj.isInstanceOf[AttackMove]) return false
+//    if (this == obj) return true
+    if (!obj.isInstanceOf[AttackMove]) return false
 
     val move: AttackMove = obj.asInstanceOf[AttackMove]
 
@@ -93,20 +103,76 @@ class AttackMove(private val board: Board, private val movedPiece: Piece, privat
   }
 }
 
-case class PawnMove(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int)
-  extends Move(board, movedPiece, destinationCoordinate) {
+class MajorAttackMove(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val attackedPiece: Piece)
+  extends AttackMove(board, destinationCoordinate, movedPiece, attackedPiece) {
+
+  override def equals(obj: Any): Boolean = {
+//    this == obj || obj.isInstanceOf[MajorAttackMove] && super.equals(obj)
+    obj.isInstanceOf[MajorAttackMove] && super.equals(obj)
+  }
+
+  override def toString: String = {
+    movedPiece.getPieceType.toString + Board.getPositionAtCoordinate(destinationCoordinate)
+  }
 }
 
-class PawnAttackMove(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int, private val attackPiece: Piece)
-  extends AttackMove(board, movedPiece, destinationCoordinate, attackPiece) {
+class PawnMove(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece)
+  extends Move(board, destinationCoordinate, movedPiece) {
+
+  override def equals(obj: Any): Boolean = {
+//    this == obj || obj.isInstanceOf[PawnMove] && super.equals(obj)
+    obj.isInstanceOf[PawnMove] && super.equals(obj)
+  }
+
+  override def toString: String = {
+    Board.getPositionAtCoordinate(destinationCoordinate)
+  }
 }
 
-case class PawnEnPassant(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int, private val attackPiece: Piece)
-  extends PawnAttackMove(board, movedPiece, destinationCoordinate, attackPiece) {
+class PawnAttackMove(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val attackPiece: Piece)
+  extends AttackMove(board, destinationCoordinate, movedPiece, attackPiece) {
+
+  override def equals(obj: Any): Boolean = {
+//    this == obj || obj.isInstanceOf[PawnAttackMove] && super.equals(obj)
+    obj.isInstanceOf[PawnAttackMove] && super.equals(obj)
+  }
+
+  override def toString: String = {
+    Board.getPositionAtCoordinate(movedPiece.getPiecePosition).substring(0, 1) + "x" + Board.getPositionAtCoordinate(destinationCoordinate)
+  }
 }
 
-case class PawnJump(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int)
-  extends Move(board, movedPiece, destinationCoordinate) {
+class PawnEnPassant(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val attackPiece: Piece)
+  extends PawnAttackMove(board, destinationCoordinate, movedPiece, attackPiece) {
+
+  override def equals(obj: Any): Boolean = {
+//    this == obj || obj.isInstanceOf[PawnEnPassant] && super.equals(obj)
+    obj.isInstanceOf[PawnEnPassant] && super.equals(obj)
+  }
+
+  override def execute: Board = {
+    val builder: Builder = new Builder()
+
+    for (piece <- board.getCurrentPlayer.getActivePieces) {
+      if (!movedPiece.equals(piece)) {
+        builder.setPiece(piece)
+      }
+    }
+
+    for (piece <- board.getCurrentPlayer.getOpponent.getActivePieces) {
+      if (!piece.equals(this.getAttackedPiece)) {
+        builder.setPiece(piece)
+      }
+    }
+
+    builder.setPiece(movedPiece.movePiece(this))
+    builder.setMoveMaker(board.getCurrentPlayer.getOpponent.getTeam)
+    builder.build
+  }
+}
+
+class PawnJump(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece)
+  extends Move(board, destinationCoordinate, movedPiece) {
   override def execute: Board = {
     val builder: Builder = new Builder()
 
@@ -126,10 +192,14 @@ case class PawnJump(private val board: Board, private val movedPiece: Piece, pri
     builder.setMoveMaker(board.getCurrentPlayer.getOpponent.getTeam)
     builder.build
   }
+
+  override def toString: String = {
+    Board.getPositionAtCoordinate(destinationCoordinate)
+  }
 }
 
-abstract class CastleMove(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int, private val castleRook: Rook, private val castleRookStart: Int, private val castleRookDestination: Int)
-  extends Move(board, movedPiece, destinationCoordinate) {
+abstract class CastleMove(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val castleRook: Rook, private val castleRookStart: Int, private val castleRookDestination: Int)
+  extends Move(board, destinationCoordinate, movedPiece) {
 
   def getCastleRook: Rook = castleRook
 
@@ -153,22 +223,49 @@ abstract class CastleMove(private val board: Board, private val movedPiece: Piec
     builder.setMoveMaker(board.getCurrentPlayer.getOpponent.getTeam)
     builder.build
   }
+
+  override def hashCode: Int = {
+    val prime = 31
+    var result = super.hashCode
+
+    result = prime * result + castleRook.hashCode
+    result = prime * result + castleRookDestination
+    result
+  }
+
+  override def equals(obj: Any): Boolean = {
+    if (this == obj) return true
+    if (!obj.isInstanceOf[CastleMove]) return false
+
+    val move: CastleMove = obj.asInstanceOf[CastleMove]
+    super.equals(move) && castleRook.equals(move.getCastleRook)
+  }
 }
 
-class KingSideCastle(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int, private val castleRook: Rook, private val castleRookStart: Int, private val castleRookDestination: Int)
-  extends CastleMove(board, movedPiece, destinationCoordinate, castleRook, castleRookStart, castleRookDestination) {
+class KingSideCastle(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val castleRook: Rook, private val castleRookStart: Int, private val castleRookDestination: Int)
+  extends CastleMove(board, destinationCoordinate, movedPiece, castleRook, castleRookStart, castleRookDestination) {
+
+  override def equals(obj: Any): Boolean = {
+    this == obj || obj.isInstanceOf[KingSideCastle] && super.equals(obj)
+  }
 
   override def toString: String = "O-O"
 }
 
-class QueenSideCastle(private val board: Board, private val movedPiece: Piece, private val destinationCoordinate: Int, private val castleRook: Rook, private val castleRookStart: Int, private val castleRookDestination: Int)
-  extends CastleMove(board, movedPiece, destinationCoordinate, castleRook, castleRookStart, castleRookDestination) {
+class QueenSideCastle(private val board: Board, private val destinationCoordinate: Int, private val movedPiece: Piece, private val castleRook: Rook, private val castleRookStart: Int, private val castleRookDestination: Int)
+  extends CastleMove(board, destinationCoordinate, movedPiece, castleRook, castleRookStart, castleRookDestination) {
+
+  override def equals(obj: Any): Boolean = {
+    this == obj || obj.isInstanceOf[QueenSideCastle] && super.equals(obj)
+  }
 
   override def toString: String = "O-O-O"
 }
 
 case class InvalidMove()
-  extends Move(null, null, -1) {
+  extends Move(null, -1) {
+
+  override def getCurrentCoordinate: Int = -1
 
   override def execute: Board = {
     throw new RuntimeException("Cannot execute an invalid move!")
